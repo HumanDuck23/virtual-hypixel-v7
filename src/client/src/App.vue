24014a6edb90caf27fa110166a3ed42ce9382348
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-04-15 18:03:19 +0200

    light / dark theme toggle

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <v-app style="height: 100vh">
     <v-main>
+      <v-app-bar color="primary">
+        <v-icon class="mr-2">
+          mdi-cable-data
+        </v-icon>
+        <v-app-bar-title>
+          Virtual Hypixel
+        </v-app-bar-title>
+        <v-spacer/>
+        <v-btn icon="mdi-brightness-4" @click="toggleTheme">Hi</v-btn>
+      </v-app-bar>
     </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
   }),
+
+  mounted() {
+    this.$vuetify.theme.current = localStorage.getItem("theme") ?? "darkTheme"
+  },
+
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.current = this.$vuetify.theme.current === "lightTheme" ? "darkTheme" : "lightTheme"
+      localStorage.setItem("theme", this.$vuetify.theme.current)
+    }
+  }
 }
 </script>
 <style>
