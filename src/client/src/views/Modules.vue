0778feb8c785efddd575cacc4250c8286fde4a15
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-04-16 12:53:14 +0200

    created module listing

--- src/client/src/views/Modules.vue
+++ src/client/src/views/Modules.vue
@@ -1,10 +1,80 @@
 <template>
-  Modules!
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-for="module in modules">
+        <v-card>
+          <v-card-title>
+            <v-icon class="mr-2">
+              {{ module.manifest.icon ?? "mdi-package-variant-closed" }}
+            </v-icon>
+            {{ module.manifest.name }}
+          </v-card-title>
+          <v-card-subtitle>
+            By {{ module.manifest.author }}
+          </v-card-subtitle>
+          <v-card-text>
+            {{ module.manifest.description }}
+          </v-card-text>
+          <v-card-actions>
+            <v-checkbox
+              v-model="moduleConfigs[module.manifest.id].enabled"
+              label="Enabled"
+              color="accent"
+              class="mb-n10"
+            ></v-checkbox>
+            <v-spacer />
+            <v-btn icon="mdi-cog-outline"></v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+
+  data: () => ({
+    modules: [],
+    moduleConfigs: {}
+  }),
+
+  mounted() {
+    this.moduleConfigs = JSON.parse(localStorage.getItem("moduleConfigs")) || {}
+    if (window.socket !== undefined) {
+      const modulePath = JSON.parse(localStorage.getItem("settings")).modules?.path || ""
+      if (modulePath !== "") {
+        window.socket.emit("getModules", modulePath)
+        window.socket.on("moduleResponse", (modules) => {
+          if (!modules) {
+            alert("The set folder doesn't exist!")
+            this.$router.push("/settings")
+          } else {
+            this.modules = modules
+            for (const module of modules) {
+              if (!this.moduleConfigs[module.manifest.id]) {
+                this.moduleConfigs[module.manifest.id] = {
+                  enabled: true
+                }
+              }
+              for (const option of Object.keys(module.config)) {
+                if (this.moduleConfigs[module.manifest.id][option] === undefined) {
+                  this.moduleConfigs[module.manifest.id][option] = ""
+                }
+              }
+            }
+            localStorage.setItem("moduleConfigs", JSON.stringify(this.moduleConfigs))
+          }
+        })
+      } else {
+        alert("Please set the path to the modules folder in the settings!")
+        this.$router.push("/settings")
+      }
+    } else {
+      console.error("socket doesnt exist")
+    }
+  }
 }
 </script>
 
